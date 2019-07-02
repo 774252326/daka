e503b37bafcee3ab90829959daa5b4279e3ce29d
unknown <you@example.com>
2019-07-02 17:33:58 +0800

    wait update

--- wait.bat
+++ wait.bat
@@ -1,4 +1,9 @@
 setlocal enableDelayedExpansion
 set /a num=%random% %% %2 + %1
 Rem set /a num=!random! %% %2 + %1
-timeout %num%
+
+for /l %%i in (1,10,%num%) do ( 
+timeout 10
+)
+
+Rem timeout %num%
